7da39398bcdbf81c2eafd4d1879ef4f2adf0de69
nealdotpy <nealpatel2021@gmail.com>
2021-02-10 18:43:01 -0500

    fresh typescript project

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p className="coming-soon-text">
+          nealpatel:portfolio. 
+          <br></br>
+          coming soon...
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
